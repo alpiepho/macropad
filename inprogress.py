3f0c2ec27122f0385cd5833aca6c1fbeb6b26ef1
apiepho <alpiepho@gmail.com>
2021-08-02 15:55:46 -0600

    setup text areas; show KEYn when pressed

--- inprogress.py
+++ inprogress.py
@@ -1,21 +1,23 @@
 import board
-import terminalio
-from adafruit_display_text import label
-import rotaryio
-
-
-# for tone
-from adafruit_macropad import MacroPad
-import time
-
-# for display
 import displayio
+import keypad
+import rotaryio
+import terminalio
+import time
+from adafruit_display_text import label  # display
+from adafruit_macropad import MacroPad   # tone
 
 
-# run tone
-# delay 100
-# run tone
-# delay 200
+
+
+
+key_pins = (board.KEY1, board.KEY2, board.KEY3, board.KEY4, board.KEY5, board.KEY6,
+            board.KEY7, board.KEY8, board.KEY9, board.KEY10, board.KEY11, board.KEY12)
+keys = keypad.Keys(key_pins, value_when_pressed=False, pull=True)
+
+encoder = rotaryio.IncrementalEncoder(board.ROTA, board.ROTB)
+
+# TODO MacroPad conflicts with keypad
 # macropad = MacroPad()
 # macropad.start_tone(988)
 # time.sleep(0.1)
@@ -24,89 +26,110 @@ import displayio
 # time.sleep(0.2)
 # macropad.stop_tone()
 
-
-encoder = rotaryio.IncrementalEncoder(board.ROTA, board.ROTB)
-
-# print title
-y = 0
-title_text = "* Adafruit Macropad *"
-title_text_area = label.Label(terminalio.FONT, text=title_text)
-title_text_area.anchor_point = (0.0, 0.0)
-title_text_area.anchored_position = (0, 0)
-
-
 # for display
+# set up (empty) text areas in a text_group
 DISPLAY_WIDTH = 128
 DISPLAY_HEIGHT = 64
-TEXT = "Key"
+text_areas = []
+y = -4
+ydelta = 18
 
-y = -4 + 20
-ydelta = DISPLAY_HEIGHT / 3 - 2
-text_area_key1 = label.Label(terminalio.FONT, text="KEY1")
-text_area_key1.anchor_point = (0.0, 0.0)
-text_area_key1.anchored_position = (0, y)
+index_line1 = len(text_areas)
+ta = label.Label(terminalio.FONT, text="")
+ta.anchor_point = (0.0, 0.0)
+ta.anchored_position = (0, y)
+text_areas.append(ta)
+y = y + ydelta
 
-text_area_key2 = label.Label(terminalio.FONT, text="KEY2")
-text_area_key2.anchor_point = (0.5, 0.0)
-text_area_key2.anchored_position = (DISPLAY_WIDTH / 2, y)
+index_line2 = len(text_areas)
+ta = label.Label(terminalio.FONT, text="")
+ta.anchor_point = (0.0, 0.0)
+ta.anchored_position = (0, y)
+text_areas.append(ta)
+y = y + ydelta
 
-text_area_key3 = label.Label(terminalio.FONT, text="KEY3")
+index_line3 = len(text_areas)
+ta = label.Label(terminalio.FONT, text="")
+ta.anchor_point = (0.0, 0.0)
+ta.anchored_position = (0, y)
+text_areas.append(ta)
+y = y + ydelta
+
+index_line4 = len(text_areas)
+ta = label.Label(terminalio.FONT, text="")
+ta.anchor_point = (0.0, 0.0)
+ta.anchored_position = (0, y)
+text_areas.append(ta)
+y = y + ydelta
+
+y = 18
+index_keys = len(text_areas)
+ta = label.Label(terminalio.FONT, text="")
+ta.anchor_point = (0.0, 0.0)
+ta.anchored_position = (0, y)
+text_areas.append(ta)
+
+ta = label.Label(terminalio.FONT, text="")
+ta.anchor_point = (0.5, 0.0)
+ta.anchored_position = (DISPLAY_WIDTH / 2, y)
+text_areas.append(ta)
+
+text_area_key3 = label.Label(terminalio.FONT, text="")
 text_area_key3.anchor_point = (1.0, 0.0)
 text_area_key3.anchored_position = (DISPLAY_WIDTH, y)
+text_areas.append(text_area_key3)
 
 y = y + ydelta
-text_area_key4 = label.Label(terminalio.FONT, text="KEY4")
+text_area_key4 = label.Label(terminalio.FONT, text="")
 text_area_key4.anchor_point = (0.0, 0.5)
 text_area_key4.anchored_position = (0, y)
+text_areas.append(text_area_key4)
 
-text_area_key5 = label.Label(terminalio.FONT, text="KEY5")
+text_area_key5 = label.Label(terminalio.FONT, text="")
 text_area_key5.anchor_point = (0.5, 0.5)
 text_area_key5.anchored_position = (DISPLAY_WIDTH / 2, y)
+text_areas.append(text_area_key5)
 
-text_area_key6 = label.Label(terminalio.FONT, text="KEY6")
+text_area_key6 = label.Label(terminalio.FONT, text="")
 text_area_key6.anchor_point = (1.0, 0.5)
 text_area_key6.anchored_position = (DISPLAY_WIDTH, y)
+text_areas.append(text_area_key6)
 
 y = y + ydelta
-text_area_key7 = label.Label(terminalio.FONT, text="KEY7")
+text_area_key7 = label.Label(terminalio.FONT, text="")
 text_area_key7.anchor_point = (0.0, 1.0)
 text_area_key7.anchored_position = (0, y)
+text_areas.append(text_area_key7)
 
-text_area_key8 = label.Label(terminalio.FONT, text="KEY8")
+text_area_key8 = label.Label(terminalio.FONT, text="")
 text_area_key8.anchor_point = (0.5, 1.0)
 text_area_key8.anchored_position = (DISPLAY_WIDTH / 2, y)
+text_areas.append(text_area_key8)
 
-text_area_key9 = label.Label(terminalio.FONT, text="KEY9")
+text_area_key9 = label.Label(terminalio.FONT, text="")
 text_area_key9.anchor_point = (1.0, 1.0)
 text_area_key9.anchored_position = (DISPLAY_WIDTH, y)
+text_areas.append(text_area_key9)
 
 y = y + ydelta
-text_area_key10 = label.Label(terminalio.FONT, text="KEY10")
+text_area_key10 = label.Label(terminalio.FONT, text="")
 text_area_key10.anchor_point = (0.0, 1.5)
 text_area_key10.anchored_position = (0, y)
+text_areas.append(text_area_key10)
 
-text_area_key11 = label.Label(terminalio.FONT, text="KEY11")
+text_area_key11 = label.Label(terminalio.FONT, text="")
 text_area_key11.anchor_point = (0.5, 1.5)
 text_area_key11.anchored_position = (DISPLAY_WIDTH / 2, y)
+text_areas.append(text_area_key11)
 
-text_area_key12 = label.Label(terminalio.FONT, text="KEY12")
+text_area_key12 = label.Label(terminalio.FONT, text="")
 text_area_key12.anchor_point = (1.0, 1.5)
 text_area_key12.anchored_position = (DISPLAY_WIDTH, y)
+text_areas.append(text_area_key12)
 
 text_group = displayio.Group()
-text_group.append(title_text_area)
-text_group.append(text_area_key1)
-text_group.append(text_area_key2)
-text_group.append(text_area_key3)
-text_group.append(text_area_key4)
-text_group.append(text_area_key5)
-text_group.append(text_area_key6)
-text_group.append(text_area_key7)
-text_group.append(text_area_key8)
-text_group.append(text_area_key9)
-text_group.append(text_area_key10)
-text_group.append(text_area_key11)
-text_group.append(text_area_key12)
+for ta in text_areas:
+    text_group.append(ta)
 
 board.DISPLAY.show(text_group)
 
@@ -114,14 +137,23 @@ board.DISPLAY.show(text_group)
 # loop
 last_position = None
 while True:
+    text_areas[index_line1].text = "* Adafruit Macropad *"
+
+
+    event = keys.events.get()
+    if event:
+        text = "KEY" + str(event.key_number + 1)
+        if event.pressed:
+            text_areas[index_keys+event.key_number].text = text
+        else:
+            text_areas[index_keys+event.key_number].text = ""
+
     position = encoder.position
     if last_position is None or position != last_position:
         last_position = position
-        # text_area_key1.text = ""
-        y = text_area_key1.anchored_position[1]
-        text_area_key1.anchored_position = (abs(position), y)
-        # board.DISPLAY.show(title_text_area)
-        board.DISPLAY.show(text_group)
+        y = text_areas[index_keys+0].anchored_position[1]
+        text_areas[index_keys+0].anchored_position = (abs(position), y)
+        # board.DISPLAY.show(text_group)
 
     # print encoder position and direction
 
