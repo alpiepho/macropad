ce31f7c3bd415c66e7fa32008ef637d9686d8c7b
apiepho <alpiepho@gmail.com>
2021-08-13 14:24:35 -0600

    more tweaks

--- timers_cp/code.py
+++ timers_cp/code.py
@@ -11,9 +11,37 @@ from rainbowio import colorwheel
 
 DELTA = 7 # in RS4020 Macropad, this approximates real time
 MAX_KEYS = 12
-BLINK_COUNT = 4
+BLINK_COUNT = 8
+ENCODER_COUNT = 1
 BRIGHTNESS_LOW = 0.2
 BRIGHTNESS_HIGH = 1.0
+
+GREEN = 0x00FF00
+YELLOW = 0xFFFF00
+ORANGE = 0xFF8C00
+RED = 0xFF0000
+
+GREEN_DIM = 0x008800
+YELLOW_DIM = 0x888800
+ORANGE_DIM = 0x884500
+RED_DIM = 0x880000
+
+class Timer():
+    delta = DELTA
+    start = 0
+    current = 0
+    running = False
+    paused = False
+    color = GREEN
+    color_dim = GREEN_DIM
+    current_yellow = 0
+    current_orange = 0
+    current_red = 0
+    blink = "_"
+    blink_on = False
+    blink_last = 0
+    sound = False
+    pressed_last = 0
 
 macropad = MacroPad()
 
@@ -24,7 +52,7 @@ macropad = MacroPad()
 # turn off pixels
 for i in range(len(macropad.pixels)):
     macropad.pixels[i] = 0x000000
-macropad.pixels.brightness = 1.0
+macropad.pixels.brightness = BRIGHTNESS_HIGH
 
 # set up (empty) text areas in a text_group
 DISPLAY_WIDTH = 128
@@ -105,7 +133,7 @@ def encoder_pressed():
     result = False
     if macropad.encoder_switch:
         encoder_pressed_count = encoder_pressed_count + 1
-        if encoder_pressed_count > 1:
+        if encoder_pressed_count > ENCODER_COUNT:
             result = True
             encoder_pressed_count = 0
     else: 
@@ -137,33 +165,20 @@ def timers_display(current):
             text_areas[index_keys+i].text = ""
             macropad.pixels[i] = 0x000000
         return
-    # for i in range(MAX_KEYS):
-    #     if i > len(timers):
-    #         text_areas[index_keys+i].text = ""
-    #         macropad.pixels[i] = 0x000000
     for i, t in enumerate(timers):
-        M = (t.current // 100) // 60
+        M = ((t.current // 100) // 60) % 60
         s = (t.current // 100) % 60
         m = (t.current % 100) // 10
-        text = f'{M:2}:{s:02}.{m:1}'
-        if t.text_last != text:
-            text_areas[index_keys+i].text = text
-            t.text_last = text
-            macropad.pixels[i] = t.color
+        text_areas[index_keys+i].text = f'{M:2}:{s:02}.{m:1}'
+        macropad.pixels[i] = t.color
 
-            # process blink
-            if t.blink == ".":
-                if (current - t.blink_last) > BLINK_COUNT:
-                    t.blink_on = not t.blink_on
-                    if not t.blink_on:
-                        macropad.pixels[i] = 0x000000
-                    t.blink_last = current
-
-def timers_dim(dim):
-    if dim:
-        macropad.pixels.brightness = BRIGHTNESS_LOW
-    else:
-        macropad.pixels.brightness = BRIGHTNESS_HIGH
+        # process blink
+        if t.blink == ".":
+            if (current - t.blink_last) > 4:
+                t.blink_on = not t.blink_on
+                if not t.blink_on:
+                    macropad.pixels[i] = t.color_dim
+                t.blink_last = current
 
 
 #############################
@@ -171,19 +186,6 @@ def timers_dim(dim):
 #############################
 timers = []
 
-class Timer():
-    delta = DELTA
-    start = 0
-    current = 0
-    running = False
-    paused = False
-    color = 0x00FF00 # green
-    blink = "_"
-    blink_on = False
-    blink_last = 0
-    sound = False
-    pressed_last = 0
-    text_last = ""
 
 def timer_add(start, delta, sound=False):
     global timers
@@ -197,6 +199,11 @@ def timer_add(start, delta, sound=False):
         t.start = start
         t.current = start
     t.sound = sound
+
+    t.current_yellow = t.start // 7
+    t.current_orange = t.start // 4
+    t.current_red = t.start // 10
+
     timers.append(t)
 
 def timer_reset(index):
@@ -236,17 +243,17 @@ def timers_update(current):
                 t.blink = "."
                 if t.delta < 0:
                     # update color
-                    # TODO: pre compute these as integers and test vs current
-                    percent = 100.0 * t.current / t.start
-                    t.color = 0x00FF00 # green
-                    if percent < 70.0 :
-                        t.color = 0xFFFF00 # yellow
-                    if percent < 40.0 :
-                        t.color = 0xFF8C00 # orange
-                    if percent < 10.0 :
-                        t.color = 0xFF0000 # red
-                    if percent < 10.0 :
-                        t.color = 0xFF0000 # red
+                    t.color = GREEN
+                    t.color_dim = GREEN_DIM
+                    if t.current < t.current_yellow:
+                        t.color = YELLOW
+                        t.color = YELLOW_DIM
+                    if t.current < t.current_orange:
+                        t.color = ORANGE
+                        t.color = ORANGE_DIM
+                    if t.current < t.current_red:
+                        t.color = RED
+                        t.color = RED_DIM
                     if t.current == 0:
                         t.blink = "_"
                         t.running = False
@@ -254,7 +261,7 @@ def timers_update(current):
                             print("play sound")
                             sound_play()
                 else:
-                    t.color = 0x00FF00 # green
+                    t.color = GREEN
             else:
                 # update color
                 t.blink = "_"       
@@ -273,11 +280,6 @@ menu_timer_sound = "off"
 menu_current_position = 0
 menu_last_position = 0
 
-def seconds_to_M_s(seconds):
-    M = (seconds) // 60
-    s = (seconds) % 60
-    return f'{M:02}:{s:02}'
-
 def check_menu(current):
     global index_line1
     global menu_state
@@ -288,9 +290,6 @@ def check_menu(current):
     global menu_timer_sound
     global menu_current_position
     global menu_last_position
-
-    if menu_state == 0:
-        return
 
     menu_last_position = menu_current_position
     menu_current_position = encoder_position()
@@ -329,14 +328,16 @@ def check_menu(current):
             if menu_timer_direction == "up":
                 timer_add(start=0, delta=DELTA, sound=False)
                 menu_timer_direction = "up"
-                menu_timer_start = 0
+                menu_timer_start = 60
                 menu_timer_sound = "off"
                 menu_state = 3
             if menu_timer_direction == "down":
                 menu_state = 5
 
     elif menu_state == 5: # timer start
-        text_areas[index_line1].text = "tmr" + str(menu_timer_index+1) + " start: " + seconds_to_M_s(menu_timer_start)
+        M = (menu_timer_start) // 60
+        s = (menu_timer_start) % 60
+        text_areas[index_line1].text = "tmr" + str(menu_timer_index+1) + " start: " + f'{M:02}:{s:02}'
         if menu_current_position > menu_last_position:
             menu_timer_start = min(3600, menu_timer_start + 1)
         if menu_current_position < menu_last_position:
@@ -381,9 +382,9 @@ def check_buttons(current):
     global timers
 
     if menu_state > 0:
-        timers_dim(dim=True)
+        macropad.pixels.brightness = BRIGHTNESS_LOW
         return
-    timers_dim(dim=False)
+    macropad.pixels.brightness = BRIGHTNESS_HIGH
     if encoder_pressed():
         timers_reset_all()
         timers = []
@@ -396,9 +397,14 @@ def check_buttons(current):
 #############################
 
 # DEBUG
+timer_add(start=0, delta=DELTA)
+timer_add(start=3000, delta=(-1*DELTA), sound=False)
 # timer_add(start=0, delta=DELTA)
 # timer_add(start=0, delta=DELTA)
-# timer_add(start=1000, delta=(-1*DELTA), sound=False)
+# timer_add(start=0, delta=DELTA)
+# timer_add(start=0, delta=DELTA)
+# timer_add(start=0, delta=DELTA)
+# timer_add(start=0, delta=DELTA)
 
 current = 0
 timers_display(current)
